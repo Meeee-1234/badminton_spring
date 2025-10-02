630584bda5d990ab78e12a4162684895e6c51e7c
Bow <thanchanok12sop@gmail.com>
2025-10-03 00:22:49 +0700

    earth bow edit

--- backend/server.js
+++ backend/server.js
@@ -75,23 +75,6 @@ const Booking = mongoose.model("Booking", bookingSchema);
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
 
-function authMiddleware(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
-
-  if (!token) {
-    return res.status(401).json({ error: "No token provided" });
-  }
-
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = decoded; // เก็บข้อมูล user ไว้ใน req
-    next();
-  } catch (err) {
-    return res.status(403).json({ error: "Invalid token" });
-  }
-}
-
 
 
 // ---------- Admin Seed ----------
